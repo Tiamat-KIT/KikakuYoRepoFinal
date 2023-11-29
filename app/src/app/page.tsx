9dbd9cc33443cee2ccdba0bb6b4f9394aa4e6893
ｳﾀｶﾀｷｮｳｽｲ <c1101081@planet.kanazawa-it.ac.jp>
2023-11-29 14:21:14 +0900

    Update page.tsx

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,19 +1,22 @@
 import Image from 'next/image'
 import {supabase} from "../supabase"
-
+import {redirect} from "next/navigation"
 export default async function Home() {
   const signIn = async (form: FormData) => {
     "use server"
     const getEmailDomain = form.get("email")!.toString().split('@')[1]
     console.log(getEmailDomain)
     if(getEmailDomain == "planet.kanazawa-it.ac.jp" || getEmailDomain == "st.kanazawa-it.ac.jp"){
-      await supabase.auth.signInWithOtp({
+      const {data,error} = await supabase.auth.signInWithOtp({
         email: form.get("email")!.toString(),
         options: {
           emailRedirectTo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/auth/callback` //`${process.env.NEXT_PUBLIC_VERCEL_URL}/show`
         }
       })
       console.log("OK!")
+      if(data !== null){
+        redirect("/show")
+      }
     }else {
       console.error("ログイン許可できません！")
     }
