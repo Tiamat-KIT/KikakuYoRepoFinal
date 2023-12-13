ef064ff993fc9c078fedd9a1e241034117745f05
Tiamat-KIT <c1101081@planet.kanazawa-it.ac.jp>
2023-12-13 06:28:19 +0000

    daisyUIでテーブルを作ります

--- app/src/app/(chakra)/show/page.tsx
+++ app/src/app/(chakra)/show/page.tsx
@@ -1,18 +1,8 @@
 "use client"
 import { useRouter,useSearchParams } from "next/navigation";
-import {
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-    Container 
-  } from '@chakra-ui/react'
+
 import TailwindContainer from "@/components/Auto/UI/TailwindContainer";
+import KTable from "@/components/Auto/UI/DataTable";
   export default function Show(){
     const router = useRouter()
     const searchParams = useSearchParams();
@@ -23,42 +13,7 @@ import TailwindContainer from "@/components/Auto/UI/TailwindContainer";
     return (
         <>
             <TailwindContainer>
-                <div className="overflow-x-auto">
-                    <table className="table">
-                        {/* head */}
-                        <thead>
-                        <tr>
-                            <th></th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {/* row 1 */}
-                        <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
-                        {/* row 2 */}
-                        <tr>
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
-                        {/* row 3 */}
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
+                <KTable />
             </TailwindContainer>
         </>
     )
--- app/src/components/Auto/UI/DataTable.tsx
+++ app/src/components/Auto/UI/DataTable.tsx
@@ -1,35 +1,59 @@
-export default function DataTable(){
+"use client"
+import { useEffect, useState,use } from "react"
 
-    const TestedItems = [{
-        name: "test",
-        time: "test"
+
+export default async function KTable(){
+    const [fetchData,setFetchData] = useState<any>(null)
+    useEffect( () => {
+        const fetchData = fetch(
+            "https://sheets.googleapis.com/v4/spreadsheets/1qOv5-b5PGoEfpr2DpI6qGFKc9htjq6qJVp4lxWL8GVQ/values/s?key=AIzaSyBBbda2OLwZWizqfk60R4BbFD8Ji4PEnV0"
+            ,{method: "GET"}).then(respomse => respomse.arrayBuffer()).then(data => data)
+        setFetchData(fetchData)
+    },[])
+    console.log(fetchData)
+    
+    const TestedItems: Record<"Grade" | "Major" | "Story",string>[] = [{
+        Grade: "3",
+        Major: "EP",
+        Story: "CG関連の試験対策で合格するためにめちゃ勉強した"
     },
     {
-        name: "test",
-        time:"test"
+        Grade: "2",
+        Major: "EP",
+        Story:"Next.jsを使えるようにするためにコードを書きまくって練習した"
     }]
     return (
-    <>
-        <table className="table"> 
-        <thead className="overflow-x-auto">
-            <tr>
-                <th />
-                <th>Name</th>
-                <th>Time</th>
-            </tr>
-        </thead>
-        <tbody>
-            {TestedItems.map((item,i) => {
-                return (
+        <div className="overflow-x-auto">
+            <table className="table table-zebra table-pin-row sm:table-sm md:table-md s">
+                <thead>
                     <tr>
-                        <th>{i}</th>
-                        <th>{item.name}</th>
-                        <th>{item.time}</th>
+                        <th>インデックス</th>
+                        <th>学年</th>
+                        <th>学科</th>
+                        <th>取り組んだこと</th>
                     </tr>
-                )
-            })}
-        </tbody>
-        </table>
-    </>
+                </thead>
+                <tbody>
+                {TestedItems.map((data,i) => {
+                    return (
+                        <tr className="divide-y-[1.5px] divide-blue-200" key={i}> 
+                            <td>
+                                {i}
+                            </td>
+                            <td>
+                                {data.Grade}
+                            </td>
+                            <td>
+                                {data.Major}
+                            </td>
+                            <td>
+                                {data.Story}
+                            </td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        </div>
     )
 }
