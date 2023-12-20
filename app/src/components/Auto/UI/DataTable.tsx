8f15b85b3dabeb05ab6488e0454dd96381fcfcda
ｳﾀｶﾀｷｮｳｽｲ <c1101081@planet.kanazawa-it.ac.jp>
2023-12-20 05:58:34 +0000

    これで動いてくれるはず

--- app/src/components/Auto/UI/DataTable.tsx
+++ app/src/components/Auto/UI/DataTable.tsx
@@ -1,19 +1,16 @@
-
-export default async function KTable(){
-    const fetchData = await fetch("/api/sps")
-            .then((response) => response.json())
-
-    
-    const TestedItems: Record<"Grade" | "Major" | "Story",string>[] = [{
-        Grade: "3",
-        Major: "EP",
-        Story: "CG関連の試験対策で合格するためにめちゃ勉強した"
-    },
-    {
-        Grade: "2",
-        Major: "EP",
-        Story:"Next.jsを使えるようにするためにコードを書きまくって練習した"
-    }]
+"use client"
+import {useState,useEffect} from "react"
+import { UseFetch, useFetch } from "use-http"
+type SpereadSheetsJson = {
+    range:string,
+    majorDimension: string,
+    values: Array<string[]>
+} 
+export default function KTable(){
+    const [fetchDetail,setDetail] = useState<SpereadSheetsJson["values"]| null>(null)
+    const {data = null} = useFetch<SpereadSheetsJson>("https://sheets.googleapis.com/v4/spreadsheets/1qOv5-b5PGoEfpr2DpI6qGFKc9htjq6qJVp4lxWL8GVQ/values/submit?key=AIzaSyBBbda2OLwZWizqfk60R4BbFD8Ji4PEnV0",{},[])
+    const result = data?.values ? data.values : [] as SpereadSheetsJson["values"]
+    setDetail(result)
     return (
         <div className="overflow-x-auto">
             <table className="table table-zebra table-pin-row sm:table-sm md:table-md s">
@@ -26,20 +23,20 @@ export default async function KTable(){
                     </tr>
                 </thead>
                 <tbody>
-                {TestedItems.map((data,i) => {
+                {fetchDetail!.map((data,i) => {
                     return (
                         <tr className="divide-y-[1.5px] divide-blue-200" key={i}> 
                             <td>
                                 {i}
                             </td>
                             <td>
-                                {data.Grade}
+                                {data[2]}
                             </td>
                             <td>
-                                {data.Major}
+                                {data[4]}{data[5]}{data[6]}
                             </td>
                             <td>
-                                {data.Story}
+                                {data[9]}
                             </td>
                         </tr>
                     )
